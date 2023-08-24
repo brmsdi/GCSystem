753692f1a51e98d39a7161ff87b592ab4ecb8e38
Wisley Bruno <srmarquesms@gmail.com>
2023-08-23 23:29:52 -0400

    Formatted lines

--- backend/src/main/java/system/gc/services/mobile/impl/MobileItemServiceImpl.java
+++ backend/src/main/java/system/gc/services/mobile/impl/MobileItemServiceImpl.java
@@ -1,6 +1,5 @@
 package system.gc.services.mobile.impl;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
