a937a851a585df4ed1563b8b851977037afbdba4
Wisley Bruno <srmarquesms@gmail.com>
2023-08-29 21:18:52 -0400

    created method forListViewMobile

--- backend/src/main/java/system/gc/dtos/ContractDTO.java
+++ backend/src/main/java/system/gc/dtos/ContractDTO.java
@@ -100,4 +100,20 @@ public class ContractDTO implements ConvertEntityAndDTO<ContractDTO, Contract> {
         }
         return contract;
     }
+
+    public static ContractDTO forListViewMobile(Contract contract) {
+        ContractDTO contractDTO = new ContractDTO();
+        contractDTO.setId(contract.getId());
+        contractDTO.setContractDate(contract.getContractDate());
+        contractDTO.setContractExpirationDate(contract.getContractExpirationDate());
+        contractDTO.setMonthlyPaymentDate(contract.getMonthlyPaymentDate());
+        contractDTO.setMonthlyDueDate(contract.getMonthlyDueDate());
+        contractDTO.setApartmentNumber(contract.getApartmentNumber());
+        contractDTO.setContractValue(contract.getContractValue());
+        contractDTO.setLessee(new LesseeDTO());
+        contractDTO.getLessee().setId(contract.getLessee().getId());
+        contractDTO.getLessee().setName(contract.getLessee().getName());
+        contractDTO.setStatus(new StatusDTO().toDTO(contract.getStatus()));
+        return contractDTO;
+    }
 }
