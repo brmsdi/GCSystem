24ef422c8966ed9087514a097f3d4e3b8b5f5466
Wisley Bruno <srmarquesms@gmail.com>
2023-08-30 22:28:33 -0400

    New fields

--- backend/src/main/java/system/gc/dtos/ContractDocumentDTO.java
+++ backend/src/main/java/system/gc/dtos/ContractDocumentDTO.java
@@ -2,13 +2,10 @@ package system.gc.dtos;
 
 import lombok.Data;
 import system.gc.entities.Contract;
-import system.gc.utils.DateUtils;
-
-import javax.validation.constraints.NotNull;
-import java.util.Date;
 
 import static system.gc.utils.DateUtils.*;
 import static system.gc.utils.NumberUtils.formatCoin;
+import static system.gc.utils.NumberUtils.valorPorExtenso;
 
 /**
  * @author Wisley Bruno Marques França
@@ -21,25 +18,32 @@ public class ContractDocumentDTO {
     private Integer id;
     private String contractDate;
     private String contractValue;
+    private String contractValueInWords;
     private int monthlyPaymentDate;
+    private String monthlyPaymentDateInWords;
     private int monthlyDueDate;
+    private String monthlyDueDateInWords;
     private String contractExpirationDate;
     private int apartmentNumber;
     private StatusDTO status;
     private CondominiumDTO condominium;
     private LesseeDTO lessee;
 
-    public ContractDocumentDTO(){
+    public ContractDocumentDTO() {
 
     }
+
     public ContractDocumentDTO(Contract contract) {
         this.setId(contract.getId());
         this.setContractDate(forView(contract.getContractDate()));
         this.setContractExpirationDate(forView(contract.getContractExpirationDate()));
         this.setMonthlyPaymentDate(contract.getMonthlyPaymentDate());
+        this.setMonthlyPaymentDateInWords(valorPorExtenso(Double.parseDouble("" + contract.getMonthlyPaymentDate())));
         this.setMonthlyDueDate(contract.getMonthlyDueDate());
+        this.setMonthlyDueDateInWords(valorPorExtenso(Double.parseDouble("" + contract.getMonthlyDueDate())));
         this.setApartmentNumber(contract.getApartmentNumber());
         this.setContractValue(formatCoin(3, 2, contract.getContractValue()));
+        this.setContractValueInWords(valorPorExtenso(contract.getContractValue()));
         LesseeDTO lesseeDTO = new LesseeDTO();
         lesseeDTO.setId(contract.getLessee().getId());
         lesseeDTO.setName(contract.getLessee().getName());
